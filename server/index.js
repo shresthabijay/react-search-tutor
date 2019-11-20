7b6a6ec4daf9964c6d5f3758d3e02cad01d374e5
Tiago Rodrigues <tiago.rodrigues@getyourguide.com>
2019-11-20 11:49:37 +0100

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 3005
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hi there!'));
 
